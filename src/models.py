df9d5abec744c306ac489fce736f27bd3c5fdee8
carlossb21 <carlosandresb21@gmail.com>
2025-06-12 00:30:43 +0000

    avance

--- src/models.py
+++ src/models.py
@@ -1,19 +1,51 @@
+from datetime import date
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import String, Boolean
+from sqlalchemy import String, Boolean, Date
 from sqlalchemy.orm import Mapped, mapped_column
 
 db = SQLAlchemy()
 
+
 class User(db.Model):
+
+    # campos
     id: Mapped[int] = mapped_column(primary_key=True)
-    email: Mapped[str] = mapped_column(String(120), unique=True, nullable=False)
-    password: Mapped[str] = mapped_column(nullable=False)
-    is_active: Mapped[bool] = mapped_column(Boolean(), nullable=False)
+    nombreUsuario: Mapped[str] = mapped_column(
+        String(15), unique=True, nullable=False)
+    email: Mapped[str] = mapped_column(
+        String(80), unique=True, nullable=False)
+    password: Mapped[str] = mapped_column(
+        String(20), nullable=False)
+
+    # def serialize(self):
+    #   return {
+    #       "id": self.id,
+    #       "email": self.email,
+    #       # do not serialize the password, its a security breach
+    #   }
 
 
-    def serialize(self):
-        return {
-            "id": self.id,
-            "email": self.email,
-            # do not serialize the password, its a security breach
-        }
+class Comentario(db.Model):
+
+    # campos
+    id: Mapped[int] = mapped_column(primary_key=True)
+    descripcion: Mapped[str] = mapped_column(String(200), nullable=False)
+    fechaCreacion: Mapped[date] = mapped_column(Date)
+    # publicacionId:
+
+
+class Adjunto(db.Model):
+
+    # campos
+    id: Mapped[int] = mapped_column(primary_key=True)
+    tipo: Mapped[str] = mapped_column(String(15), nullable=False)
+    # publicacionId:
+
+
+class Publicacion(db.Model):
+
+    # campos
+    id: Mapped[int] = mapped_column(primary_key=True)
+    descripcion: Mapped[str] = mapped_column(String(200), nullable=False)
+    fechaCreacion: Mapped[date] = mapped_column(Date)
+    #usuarioId:
